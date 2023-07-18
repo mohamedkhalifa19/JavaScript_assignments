c1e87ef4931d8d82eb87f03255958fb125f43379
Mohamed Khalifa <91298305+mohamedkhalifa19@users.noreply.github.com>
2023-07-18 23:31:17 +0300

    Create basicOperators.js

---
+++ basicOperators.js
@@ -0,0 +1,12 @@
+let population = 100 * 10**6;
+let half = population/2;
+let Finland = 6 *10**6;
+let averageCountry= 33 *10**6;
+let description = 'Portugal is in Europe, and its 11 million\
+people speak portuguese';
+console.log(half);
+++population;
+console.log(population);
+console.log(population>Finland);
+console.log(population<averageCountry);
+console.log(description);
