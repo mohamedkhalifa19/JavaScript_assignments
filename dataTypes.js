469dd5a0e71264f8191d6593e6667d7d3838a88d
Mohamed Khalifa <91298305+mohamedkhalifa19@users.noreply.github.com>
2023-07-18 09:06:34 +0300

    Create dataTypes.js

---
+++ dataTypes.js
@@ -0,0 +1,9 @@
+let country = "Egypt";
+let continent ="Africa";
+let population=109.26*Math.pow(10,6);
+let isIsland = false;
+let language;
+console.log(typeof country);
+console.log(typeof population);
+console.log(typeof isIsland);
+console.log(typeof language);
