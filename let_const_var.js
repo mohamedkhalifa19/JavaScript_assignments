a706c2e6c2bdf6ac79802c88325834c680d68e45
Mohamed Khalifa <91298305+mohamedkhalifa19@users.noreply.github.com>
2023-07-18 09:25:39 +0300

    Create let_const_var.js

---
+++ let_const_var.js
@@ -0,0 +1,11 @@
+const country = "Egypt";
+const continent ="Africa";
+let population=109.26*Math.pow(10,6);
+const isIsland = false;
+const language ="Arabic";
+console.log( country);
+console.log( continent);
+console.log( population);
+console.log( isIsland);
+console.log( language);
+
