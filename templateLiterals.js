24d84cf4877590a8184bd2273c81eeca00e15c4a
Mohamed Khalifa <91298305+mohamedkhalifa19@users.noreply.github.com>
2023-07-19 05:44:31 +0300

    Create templateLiterals.js

---
+++ templateLiterals.js
@@ -0,0 +1,6 @@
+let population = 105;
+let country = 'Egypt';
+let continent ='Africa';
+let language = 'Arabic';
+let description = `${country} is in ${continent}, and its ${population} million people speak ${language}`;
+console.log(description);
