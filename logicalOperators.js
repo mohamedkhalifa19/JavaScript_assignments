d6de9567c7c4ee73584d464f304829659b336cec
Mohamed Khalifa <91298305+mohamedkhalifa19@users.noreply.github.com>
2023-07-20 19:08:21 +0300

    Create logicalOperators.js

---
+++ logicalOperators.js
@@ -0,0 +1,9 @@
+const isIsland = false;
+let population =105;
+const language ="Arabic" ;
+
+if(!isIsland &&population<50&&language=="english"){
+    console.log("You should live in Egypt ❤");
+} else{
+    console.log('Egypt does not meet your criteria 😥');
+}
