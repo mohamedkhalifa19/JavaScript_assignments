3a7bf78e96f6f6cf0090ad33ac6d49e82bbbae18
Mohamed Khalifa <91298305+mohamedkhalifa19@users.noreply.github.com>
2023-07-22 17:54:12 +0300

    Create arrowFunctions

---
+++ arrowFunctions.js
@@ -0,0 +1,13 @@
+'use strict';
+let worldPopulation = 7900;
+
+
+let percentageOfWorld3 = population=> `${(population / worldPopulation *100).toFixed(2)} %`;
+
+
+console.log(percentageOfWorld3(1441));
+console.log(percentageOfWorld3(105));
+console.log(percentageOfWorld3(15));
+
+
+age=>age*2;
